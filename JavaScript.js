f4850101b4ea26d61f80e818107fb457b5f0313f
Kicsi.T1 <totin.2citromail.hu@gmail.com>
2025-11-30 13:19:56 +0100

    Javitotam egy vizualis hibat es hozadatam meg az About us es a solutions section-t a projecthez

--- JavaScript.js
+++ JavaScript.js
@@ -3,18 +3,66 @@ function DropDown(TagId,ArrowId)
     var Tag = document.getElementById(TagId);
     var img = document.getElementById("HamburgerImg");
     var Arrow=document.getElementById(ArrowId)
-    if (Tag.style.display === "block") {
-        if (Tag) Tag.style.display = "none";
-        if (img) img.src = "icons/Hamburger menu.png";
-        if (Arrow) Arrow.src="icons/chevron-down, arrow-down.png";
-    } 
-    else 
+  
+    switch(TagId)
     {
-        if (Tag) Tag.style.display = "block";
-        if (img )img.src = "icons/Hamburgerx.png";
-        if (Arrow) Arrow.src="icons/chevron-up, arrow-up.png";
+        case "phone_drop_down_menu":
+            if (Tag.style.display === "block") 
+            {
+                if (Tag) Tag.style.display = "none";
+                if (img) img.src = "icons/Hamburger menu.png";
+            } 
+            else 
+            {
+                if (Tag) Tag.style.display = "block";
+                if (img )img.src = "icons/Hamburgerx.png";
+            }
+        break;
+
+        case "pddc1":
+            if (Tag.style.display === "block") {
+                if (Tag) Tag.style.display = "none";
+                if (Arrow) Arrow.src="icons/chevron-down, arrow-down.png";
+            } 
+            else 
+            {
+                if (Tag) Tag.style.display = "block";
+                if (Arrow) Arrow.src="icons/chevron-up, arrow-up.png";
+            }
+        break;
+
+        case "pddc2":
+            if (Tag.style.display === "block") {
+                if (Tag) Tag.style.display = "none";
+                if (Arrow) Arrow.src="icons/chevron-down, arrow-down.png";
+            } 
+            else 
+            {
+                if (Tag) Tag.style.display = "block";
+                if (Arrow) Arrow.src="icons/chevron-up, arrow-up.png";
+            }
+        break;
+
+         case "pddc3":
+            if (Tag.style.display === "block") {
+                if (Tag) Tag.style.display = "none";
+                if (Arrow) Arrow.src="icons/chevron-down, arrow-down.png";
+            } 
+            else 
+            {
+                if (Tag) Tag.style.display = "block";
+                if (Arrow) Arrow.src="icons/chevron-up, arrow-up.png";
+            }
+        break;
+
+        default:
+            console.log("Nem jo tag-et adtal meg!")    
     }
 }
+
+
+
+
 
 window.addEventListener('resize', function() {
     if (window.innerWidth > 1200) {
