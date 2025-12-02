bc404d47c02e1e9761d6d00d39cd9a842714c7af
Kicsi.T1 <totin.2citromail.hu@gmail.com>
2025-12-02 23:56:02 +0100

    A Dar mode mukodik es az img-k lettek rendes font-awesom-os icono-ra cserelve.

--- JavaScript.js
+++ JavaScript.js
@@ -1,8 +1,10 @@
-function DropDown(TagId,ArrowId)
+function DropDown(TagId,ArrowDownID,ArrowUpID)
 {
-    var Tag = document.getElementById(TagId);
-    var img = document.getElementById("HamburgerImg");
-    var Arrow=document.getElementById(ArrowId)
+    const Tag = document.getElementById(TagId);
+    const IHamburgerIcon = document.getElementById("HamburgerIcon");
+    const IHamburgerIconx = document.getElementById("HamburgerIconx");
+    const ArrowDown = document.getElementById(ArrowDownID)
+    const ArrowUp = document.getElementById(ArrowUpID)
   
     switch(TagId)
     {
@@ -10,63 +12,94 @@ function DropDown(TagId,ArrowId)
             if (Tag.style.display === "block") 
             {
                 if (Tag) Tag.style.display = "none";
-                if (img) img.src = "icons/Hamburger menu.png";
+
+                if(IHamburgerIcon)      IHamburgerIcon.style.display = "block";
+                if(IHamburgerIconx)     IHamburgerIconx.style.display = "none";       
             } 
             else 
             {
                 if (Tag) Tag.style.display = "block";
-                if (img )img.src = "icons/Hamburgerx.png";
+
+                 if(IHamburgerIcon)     IHamburgerIcon.style.display = "none";
+                 if(IHamburgerIconx)    IHamburgerIconx.style.display = "block"; 
             }
         break;
 
         case "pddc1":
             if (Tag.style.display === "block") {
-                if (Tag) Tag.style.display = "none";
-                if (Arrow) Arrow.src="icons/chevron-down, arrow-down.png";
+                if (Tag)    Tag.style.display = "none";
+                if(ArrowDown)   ArrowDown.style.display = "block";
+                if(ArrowUp)     ArrowUp.style.display = "none";
             } 
             else 
             {
                 if (Tag) Tag.style.display = "block";
-                if (Arrow) Arrow.src="icons/chevron-up, arrow-up.png";
+                if(ArrowDown)   ArrowDown.style.display = "none";
+                if(ArrowUp)     ArrowUp.style.display = "block";
             }
         break;
 
         case "pddc2":
             if (Tag.style.display === "block") {
                 if (Tag) Tag.style.display = "none";
-                if (Arrow) Arrow.src="icons/chevron-down, arrow-down.png";
+                if(ArrowDown)   ArrowDown.style.display = "block";
+                if(ArrowUp)     ArrowUp.style.display = "none";
             } 
             else 
             {
                 if (Tag) Tag.style.display = "block";
-                if (Arrow) Arrow.src="icons/chevron-up, arrow-up.png";
+                if(ArrowDown)   ArrowDown.style.display = "none";
+                if(ArrowUp)     ArrowUp.style.display = "block";
             }
         break;
 
          case "pddc3":
             if (Tag.style.display === "block") {
                 if (Tag) Tag.style.display = "none";
-                if (Arrow) Arrow.src="icons/chevron-down, arrow-down.png";
+                if(ArrowDown)   ArrowDown.style.display = "block";
+                if(ArrowUp)     ArrowUp.style.display = "none";
             } 
             else 
             {
                 if (Tag) Tag.style.display = "block";
-                if (Arrow) Arrow.src="icons/chevron-up, arrow-up.png";
+                if(ArrowDown)   ArrowDown.style.display = "none";
+                if(ArrowUp)     ArrowUp.style.display = "block";
             }
         break;
 
         default:
             console.log("Nem jo tag-et adtal meg!")    
+        }
+}
+function ModeSwitching()
+{
+    const SunIcon = document.getElementById("IconSun");
+    const MoonIcon = document.getElementById("IconMoon");
+    const DarkModeLink = document.getElementById("DarkMode");
+    if(MoonIcon.style.display === "block")
+    {
+        MoonIcon.style.display = "none";
+        SunIcon.style.display = "block"
+        DarkModeLink.disabled = false;
+    } 
+    else
+    {
+        MoonIcon.style.display = "block";
+        SunIcon.style.display = "none";
+        DarkModeLink.disabled = true;
     }
 }
 
 
 
 
-
 window.addEventListener('resize', function() {
     if (window.innerWidth > 1200) {
-        document.querySelector('.phone_drop_down_menu').style.display = 'none';
-        document.getElementById("HamburgerImg").src="icons/Hamburger menu.png";
+        const menu = document.getElementById("phone_drop_down_menu");
+        const bars = document.getElementById("HamburgerIcon");
+        const x = document.getElementById("HamburgerIconx");
+        menu.style.display = 'none';
+        bars.style.display = "block";
+        x.style.display = "none";
     }
 });
